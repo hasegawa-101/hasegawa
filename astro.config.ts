ab7c25d38da7358776b6b9ade4ddd09cc59e8909
hasegawa-101 <hasegawa@101de-sign.com>
2024-03-02 22:16:35 +0900

    fix: サイトのルートURL設定を変更

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://hasegawa-101.github.io/",
+  site: "https://hasegawa-101.github.io/hasegawa/",
   integrations: [tailwind({ nesting: true })],
 });
