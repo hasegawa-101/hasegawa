a28837afb4f339b005a9cdfad46cbd881ff525d4
hasegawa-101 <hasegawa@101de-sign.com>
2024-03-03 21:39:47 +0900

    feat: ドメインを変更対応

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://hasegawa-101.github.io",
-  base: "/hasegawa",
+  site: "https://hayatohasegawa.com",
   integrations: [tailwind({ nesting: true })],
 });
