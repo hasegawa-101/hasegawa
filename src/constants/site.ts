74fd5a5f17e0a8f9380df56fa95eb5bd87a4653a
hayato hasegawa <hasegawa@101de-sign.com>
2024-11-06 18:52:45 +0900

    📦 ESLint、Prettier を Biome に置き換え

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Hayato Hasegawa - 長谷川 駿";
 export const SITE_DESCRIPTION =
-  "長谷川 駿のホームページです。フロントエンド領域とデザインを齧っています。";
+	"長谷川 駿のホームページです。フロントエンド領域とデザインを齧っています。";
 export const SITE_URL = "https://hayato.io";
