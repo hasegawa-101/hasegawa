b5b1b777ca60ff15646caef105b10759113d26c7
hayato hasegawa <hasegawa@101de-sign.com>
2024-11-09 22:28:57 +0900

    🎨 コードフォーマット

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      date: z.date(),
-      ogImage: image().optional(),
-    }),
+const blogCollection = defineCollection({
+	type: "content",
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			date: z.date(),
+			ogImage: image().optional(),
+		}),
 });
 
 export const collections = {
-  blog,
+	blog: blogCollection,
 };
