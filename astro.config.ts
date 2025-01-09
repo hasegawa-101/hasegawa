7916f38bd1ea4fd952a281901477f72d8464e181
hasegawa-101 <hase1225hayato@gmail.com>
2025-01-10 01:35:20 +0900

    feat: 記事コンテンツ更新をmdxでもできるようにした

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://hayatohasegawa.com",
-  integrations: [tailwind({ nesting: true })],
+  integrations: [tailwind({ nesting: true }), mdx()],
 });
