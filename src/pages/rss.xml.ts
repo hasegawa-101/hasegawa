2ea8aa1ed0ed5b51592a5f3244d31cb0939c3973
Hayato Hasegawa <hasegawa_hayato@anyland.jp>
2025-07-23 22:32:17 +0900

    feat: スタイルの調整や内容の更新

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,25 +1,23 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import type { APIContext } from 'astro';
-import sanitizeHtml from 'sanitize-html';
-import MarkdownIt from 'markdown-it';
+import { type CollectionEntry, getCollection } from "astro:content";
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import MarkdownIt from "markdown-it";
+import sanitizeHtml from "sanitize-html";
 const parser = new MarkdownIt();
 
 export async function GET(context: APIContext) {
+	const blog = await getCollection("blog");
 
-  const blog = await getCollection('blog');
-
-  return rss({
-    title: 'Hayato Hasegawa',
-    description: '長谷川駿のウェブサイトです。',
-    site: context.site!,
-    items: blog.map((post: any) => ({
-      title: post.data.title,
-      pubDate: post.data.date,
-      description: post.data.description,
-      link: `/blog/${post.slug}`,
-      content: sanitizeHtml(parser.render(post.body)),
-    })),
-  
-  });
+	return rss({
+		title: "Hayato Hasegawa",
+		description: "長谷川駿のウェブサイトです。",
+		site: context.site ? context.site : "https://hayatohasegawa.com",
+		items: blog.map((post: CollectionEntry<"blog">) => ({
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.description,
+			link: `/blog/${post.slug}`,
+			content: sanitizeHtml(parser.render(post.body)),
+		})),
+	});
 }
