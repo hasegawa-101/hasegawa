64f02c5cb7b5e7bdea7743ccea8b8dd2d3a36226
Hayato Hasegawa <hasegawa_hayato@anyland.jp>
2025-10-12 02:34:58 +0900

    feat: tailwindcss v4 へアップグレード

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   site: "https://hayatohasegawa.com",
-  integrations: [tailwind({ nesting: true }), mdx()],
+  integrations: [mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
